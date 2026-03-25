0401a616a574e1a3d4e807bfb48ed9411373e9e2
Jigar Subhash Rajput <80737219+JigarRajput@users.noreply.github.com>
2026-03-26 01:40:24 +0530

    Update NavigationContext.java
    Added Comments to refer later

--- behavioral/strategy/NavigationContext.java
+++ behavioral/strategy/NavigationContext.java
@@ -1,15 +1,19 @@
 package behavioral.strategy;
 
 public class NavigationContext {
+    // The currently selected strategy (algorithm).
+    // Context delegates the navigation behavior to this object.
     NavigationStrategy strategy;
 
     public NavigationContext(NavigationStrategy strategy) {}
     public NavigationContext() {}
 
+    // Allows changing the algorithm at runtime
     public void setNavigationStrategy(NavigationStrategy navigationStrategy) {
         this.strategy = navigationStrategy;
     }
 
+    // Delegates the navigation work to the chosen strategy
     public void executeNavigation(Point a, Point b) {
         strategy.navigate(a, b);
     }
